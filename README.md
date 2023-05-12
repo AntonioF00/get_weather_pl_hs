8f3ae30025cfed213506c7ee5aefb9ee9c4557f5
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2023-05-12 11:16:25 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # get_weather_pl_hs
+
+SWI-Prolog
+
+https://www.haskell.org/ghcup/
+
+cabal install http-simple
--- get-weather.hs
+++ get-weather.hs
@@ -1,6 +1,9 @@
+-- cabal update
+-- cabal install HTTP-Simple
+-- cabal install mtl
+
+import Network.Wreq 
 import Control.Monad.State
-import Network.HTTP.Client
-import Network.HTTP.Client.TLS
 
 data AppState = AppState
   { latitude :: Maybe String
@@ -8,30 +11,25 @@ data AppState = AppState
   , url :: Maybe String
   }
 
-initializeLatLon :: State AppState ()
+initializeLatLon :: StateT AppState IO ()
 initializeLatLon = do
-  put (AppState { latitude = Nothing, longitude = Nothing, url = Nothing })
+  put $ AppState { latitude = Nothing, longitude = Nothing, url = Nothing }
   lat <- liftIO $ putStr "Enter latitude: " >> getLine
   lon <- liftIO $ putStr "Enter longitude: " >> getLine
-  modify (\s -> s { latitude = Just lat, longitude = Just lon })
+  modify $ \s -> s { latitude = Just lat, longitude = Just lon }
 
-createUrl :: State AppState ()
+createUrl :: StateT AppState IO ()
 createUrl = do
-  lat <- gets latitude
-  lon <- gets longitude
-  case (lat, lon) of
-    (Just lat', Just lon') ->
-      modify (\s -> s { url = Just $ "https://api.openweathermap.org/data/2.5/weather?lat=" ++ lat' ++ "&lon=" ++ lon' ++ "&appid=9525a467ef22974bc25346d4bc02de69" })
-    _ -> return ()
-
-getUrl :: AppState -> Maybe String
-getUrl = url
+  liftM2 (,) (gets latitude) (gets longitude) >>= \(lat, lon) ->
+    let url' = "https://api.openweathermap.org/data/2.5/weather?lat=" ++ lat ++ "&lon=" ++ lon ++ "&appid=9525a467ef22974bc25346d4bc02de69"
+     in modify $ \s -> s { url = Just url' }
 
 getWeatherData :: IO ()
 getWeatherData = do
   let initialState = AppState { latitude = Nothing, longitude = Nothing, url = Nothing }
-      finalState = execState (initializeLatLon >> createUrl) initialState
-  case getUrl finalState of
+      finalState = execStateT (initializeLatLon >> createUrl) initialState
+      getUrl' = url
+  case getUrl' finalState of
     Just url' -> do
       manager <- newTlsManager
       request <- parseRequest url'
@@ -41,12 +39,3 @@ getWeatherData = do
 
 main :: IO ()
 main = getWeatherData
-
-
---test
--- $ runhaskell get-weather.hs
-
---1
---Enter latitude: 37.7749
---Enter longitude: -122.4194
---{"coord":{"lon":-122.42,"lat":37.77},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"base":"stations","main":{"temp":285.22,"feels_like":284.42,"temp_min":283.95,"temp_max":286.15,"pressure":1014,"humidity":65},"visibility":10000,"wind":{"speed":3.6,"deg":290},"clouds":{"all":1},"dt":1555501564,"sys":{"type":1,"id":5122,"message":0.0139,"country":"US","sunrise":1555471623,"sunset":1555520683},"timezone":-25200,"id":420006353,"name":"San Francisco","cod":200}
